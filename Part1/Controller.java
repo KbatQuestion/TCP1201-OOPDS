082dbc0d1f1e580bb508a1e9085af4a7aacfe4e0
KbatQuestion <99158684+KbatQuestion@users.noreply.github.com>
2023-12-25 17:41:46 +0800

    UserLogin

--- Part1/Controller.java
+++ Part1/Controller.java
@@ -1,7 +1,23 @@
 package Part1;
 
+import javafx.event.ActionEvent;
+import javafx.scene.control.TextField;
+
 public class Controller {
-    
+
+    public void isInt(TextField input, String password) {
+
+        String passWord = password;
+
+        if (passWord.equals("deez")) {
+            System.out.println("hello");
+
+        }
+
+        else {
+            System.out.println("Nutz");
+        }
+    }
 }
 
-//Where main logic of the program will be made
+// Where main logic of the program will be made
--- Part1/View.java
+++ Part1/View.java
@@ -1,6 +1,75 @@
 package Part1;
-public class View {
 
-    //where the Gui will be made
-    
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.geometry.Insets;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
+public class View extends Application {
+
+    Button button;
+    Stage window;
+    Scene scene1, scene2;
+
+    public static void main(String[] args) {
+        launch(args);
+
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        Controller controller = new Controller();
+        window = primaryStage;
+        window.setTitle("Log in");
+
+        GridPane grid = new GridPane();
+        grid.setPadding(new Insets(10, 10, 10, 10));
+        grid.setVgap(8);
+        grid.setHgap(10);
+
+        // lable name
+        Label nameLable = new Label("ID Number");
+        GridPane.setConstraints(nameLable, 2, 0);
+
+        // name imput
+
+        TextField namTextField = new TextField();
+        namTextField.setPromptText("Eg: 12334567890");
+        GridPane.setConstraints(namTextField, 2, 1);
+
+        // Password name
+        Label passLable = new Label("Password");
+        GridPane.setConstraints(passLable, 2, 3);
+
+        // Password imput
+
+        TextField passTextField = new TextField();
+        GridPane.setConstraints(passTextField, 2, 4);
+
+        // Log In Button
+
+        Button loginButton = new Button("Log in");
+        loginButton.setOnAction(e -> controller.isInt(passTextField, passTextField.getText()));
+
+        GridPane.setConstraints(loginButton, 2, 5);
+
+        // Passing the Object to the Scene
+
+        grid.getChildren().addAll(nameLable, namTextField, passLable, passTextField, loginButton);
+
+        Scene scene = new Scene(grid, 400, 200);
+        window.setScene(scene);
+
+        window.show();
+
+    }
+
 }
